eb7661614e2f4afababcd6521535d4676cd7a3a9
Philip Johansson <philip.hlm@live.se>
2026-07-07 21:24:53 +0200

    feat: expandable allows for objects to be expanded inline in configuration too

--- ts/packages/core/src/schema.ts
+++ ts/packages/core/src/schema.ts
@@ -1,16 +1,19 @@
 import {z} from "zod";
 
 export const expandable = <T extends z.ZodTypeAny>(target: T, parse: (data: string) => z.input<T> = (data: string) => JSON.parse(data)) => (
-    z.codec(z.string(), target, {
-        decode: (s, ctx) => {
-            try { return parse(s) as z.input<T> }
-            catch (e) {ctx.issues.push({
-                code: "invalid_format", format: "custom", input: s, message: String(e) });
-                return z.NEVER
+    z.union([
+        target,
+        z.codec(z.string(), target, {
+            decode: (s, ctx) => {
+                try { return parse(s) as z.input<T> }
+                catch (e) {ctx.issues.push({
+                    code: "invalid_format", format: "custom", input: s, message: String(e) });
+                    return z.NEVER
+                }
+            },
+            encode: (v) => {
+                throw Error("encode is not supported")
             }
-        },
-        encode: (v) => {
-            throw Error("encode is not supported")
-        }
-    })
+        })
+    ])
 )
